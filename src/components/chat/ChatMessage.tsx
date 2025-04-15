c249efc017fec65f804b17ecda4cbe5c79d29c2a
ArtemKhoda <khodakivskiya@gmail.com>
2025-04-15 17:58:17 -0400

    add kosher item

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -65,7 +65,7 @@ const ChatMessage: React.FC<ChatMessageProps> = ({
           badgeInset="14%"
           color="success"
         >
-          { isRequest ? (<PersonIcon fontSize="large" color="primary" />) : (<div style={{display: 'flex', alignItems: 'center'}}><img src="/restaurant-depot/restaruantlogo.png" alt="logo" style={{width: "40px", height: "40px"}}/></div>)}
+          { isRequest ? (<PersonIcon fontSize="large" color="primary" />) : (<div style={{display: 'flex', alignItems: 'center'}}><img src="/kosher-groceries/tomko-logo.png" alt="logo" style={{width: "60px", height: "45px"}}/></div>)}
         </Badge>
         {
           isRequest ? (<p></p>) : (<p style={{paddingLeft: '5px'}}>Assistant</p>)
