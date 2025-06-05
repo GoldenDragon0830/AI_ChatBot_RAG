a8e9881e2d4ee09272f2a6e25ee67e2b5ee1dc97
denysbakin <denysbakin@gmail.com>
2025-06-05 13:23:10 -0400

    make initial structure for limb lengthening

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -72,13 +72,13 @@ const ChatMessage: React.FC<ChatMessageProps> = ({ content, role, imageUrl }) =>
           >
             {isRequest ? (
               <img
-                src="/west-side-wok/user.png"
+                src="/limblengthening/user.png"
                 alt="logo"
                 style={{ width: "40px", height: "40px", maxWidth: "40px", minWidth: "40px" }}
               />
             ) : (
               <img
-                src="/west-side-wok/assistant.png"
+                src="/limblengthening/assistant.png"
                 alt="logo"
                 style={{ width: "40px", height: "40px", maxWidth: "40px", minWidth: "40px" }}
               />
