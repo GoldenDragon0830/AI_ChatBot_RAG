43a5eae1e9e029d8d27dc8d6f689fed461e936f6
denysbakin <denysbakin@gmail.com>
2025-06-17 12:07:13 -0400

    handle email feature for apointment and contract form

--- src/components/chat/AppointmentForm.tsx
+++ src/components/chat/AppointmentForm.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+
 import {
   Box,
   Button,
@@ -16,125 +18,241 @@ import {
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
 
+const EMAIL_SERVICE_ID = "service_zeoucv3"
+const EMAIL_TEMPLATE_ID = "template_qa57d3p"
+const EMAIL_USER_PUBLIC_KEY = "zLRxsQKdzbmCEjQfx"
+
 const AppointmentForm = () => {
+
+  const [formData, setFormData] = useState({
+     doctor: '',
+     patientCondition: [],
+     patientInterest: [],
+     patientPreference: '',
+     firstName: '',
+     lastName: '',
+     email: '',
+     phone: ''
+  });
+
+  const sendEmail = (formData: any) => {
+    const templateParams = {
+      doctor: formData.doctor,
+      patientCondition: formData.patientCondition,
+      patientInterest: formData.patientInterest,
+      patientPreference: formData.patientPreference,
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      phone: formData.phone
+    };
+
+    emailjs.send(EMAIL_SERVICE_ID, EMAIL_TEMPLATE_ID, templateParams, EMAIL_USER_PUBLIC_KEY)
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      }, (error) => {
+        console.log('FAILED...', error);
+      });
+  }
+
+  const handleInputChange = (e: any) => {
+    const { name, value, checked, type } = e.target;
+
+    if (type === 'checkbox') {
+      setFormData((prevState : any) => {
+        const currentArray = prevState[name];
+        if (checked) {
+          return {
+            ...prevState,
+            [name]: [...currentArray, value],
+          };
+        } else {
+          return {
+            ...prevState,
+            [name]: currentArray.filter((item: string) => item !== value),
+          };
+        }
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
+
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    sendEmail(formData);
+  };
+
   return (
     <Box sx={{ p: 3, bgcolor: 'white', borderRadius: 2, boxShadow: 3, maxWidth: 900, mx: 'auto' }}>
       <Typography variant="h5" sx={{ mb: 2, fontWeight: 'bold', color: '#73AD21' }}>
         Request an Appointment
       </Typography>
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={6}>
-          <Grid container spacing={2}>
-            {/* Form Fields */}
-            <Grid item xs={12}>
-              <FormControl fullWidth>
-                <FormLabel>Which doctor would you like to see?</FormLabel>
-                <Select defaultValue="">
-                  <MenuItem value="">Choose one</MenuItem>
-                  <MenuItem value="Dr. Robert Rozbruch">Dr. Robert Rozbruch</MenuItem>
-                  <MenuItem value="Dr. Austin Fragomen">Dr. Austin Fragomen</MenuItem>
-                  <MenuItem value="Dr. Taylor Reif">Dr. Taylor Reif</MenuItem>
-                  <MenuItem value="Dr. Jason Hoellwarth">Dr. Jason Hoellwarth</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl component="fieldset">
-                <FormLabel component="legend">Patient Condition</FormLabel>
-                <FormGroup row>
-                  <FormControlLabel control={<Checkbox />} label="Bowleg" />
-                  <FormControlLabel control={<Checkbox />} label="Knock Knee" />
-                  <FormControlLabel control={<Checkbox />} label="Osseointegration" />
-                  <FormControlLabel control={<Checkbox />} label="Leg Length Discrepancy" />
-                  <FormControlLabel control={<Checkbox />} label="Arm or Wrist" />
-                  <FormControlLabel control={<Checkbox />} label="Foot or Ankle" />
-                  <FormControlLabel control={<Checkbox />} label="Other/Something Else" />
-                  <FormControlLabel control={<Checkbox />} label="I Don't Know" />
-                </FormGroup>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl component="fieldset">
-                <FormLabel component="legend">Patient Interest</FormLabel>
-                <FormGroup row>
-                  <FormControlLabel control={<Checkbox />} label="Appointment" />
-                  <FormControlLabel control={<Checkbox />} label="Consultation" />
-                  <FormControlLabel control={<Checkbox />} label="Surgery" />
-                  <FormControlLabel control={<Checkbox />} label="I Don't Know" />
-                </FormGroup>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12}>
-              <FormControl fullWidth>
-                <FormLabel>Patient Preference</FormLabel>
-                <Select defaultValue="">
-                  <MenuItem value="">Choose one</MenuItem>
-                  <MenuItem value="person">In person</MenuItem>
-                  <MenuItem value="virtual">Virtual</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={6}>
-              <TextField fullWidth label="First Name" />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField fullWidth label="Last Name" />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField fullWidth label="Email" />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField fullWidth label="Phone" />
-            </Grid>
-            <Grid item xs={12}>
-              <Button variant="contained" color="success" fullWidth>
-                Submit
-              </Button>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Grid container spacing={2}>
+              {/* Form Fields */}
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <FormLabel>Which doctor would you like to see?</FormLabel>
+                  <Select 
+                    name='doctor'
+                    defaultValue=""
+                    value={formData.doctor}
+                    onChange={handleInputChange}
+                    >
+                    <MenuItem value="">Choose one</MenuItem>
+                    <MenuItem value="Dr. Robert Rozbruch">Dr. Robert Rozbruch</MenuItem>
+                    <MenuItem value="Dr. Austin Fragomen">Dr. Austin Fragomen</MenuItem>
+                    <MenuItem value="Dr. Taylor Reif">Dr. Taylor Reif</MenuItem>
+                    <MenuItem value="Dr. Jason Hoellwarth">Dr. Jason Hoellwarth</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Patient Condition</FormLabel>
+                  <FormGroup row>
+                    <FormControlLabel
+                      control={<Checkbox name="patientCondition" value="Bowleg" onChange={handleInputChange} />}
+                      label="Bowleg"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox name="patientCondition" value="Knock Knee" onChange={handleInputChange} />}
+                      label="Knock Knee"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox name="patientCondition" value="Osseointegration" onChange={handleInputChange} />}
+                      label="Osseointegration"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox name="patientCondition" value="Leg Length Discrepancy" onChange={handleInputChange} />}
+                      label="Leg Length Discrepancy"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox name="patientCondition" value="Arm or Wrist" onChange={handleInputChange} />}
+                      label="Arm or Wrist"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox name="patientCondition" value="Foot or Ankle" onChange={handleInputChange} />}
+                      label="Foot or Ankle"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox name="patientCondition" value="Other/Something Else" onChange={handleInputChange} />}
+                      label="Other/Something Else"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox name="patientCondition" value="I Don't Know" onChange={handleInputChange} />}
+                      label="I Don't Know"
+                    />
+                  </FormGroup>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Patient Interest</FormLabel>
+                  <FormGroup row>
+                    <FormControlLabel
+                      control={<Checkbox name="patientInterest" value="Appointment" onChange={handleInputChange} />}
+                      label="Appointment"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox name="patientInterest" value="Consultation" onChange={handleInputChange} />}
+                      label="Consultation"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox name="patientInterest" value="Surgery" onChange={handleInputChange} />}
+                      label="Surgery"
+                    />
+                    <FormControlLabel
+                      control={<Checkbox name="patientInterest" value="I Don't Know" onChange={handleInputChange} />}
+                      label="I Don't Know"
+                    />
+                  </FormGroup>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <FormLabel>Patient Preference</FormLabel>
+                  <Select 
+                    defaultValue=""
+                    name='patientPreference'
+                    onChange={handleInputChange}
+                    value={formData.patientPreference}
+                    >
+                    <MenuItem value="">Choose one</MenuItem>
+                    <MenuItem value="person">In person</MenuItem>
+                    <MenuItem value="virtual">Virtual</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={6}>
+                <TextField fullWidth label="First Name"  name='firstName' value={formData.firstName} onChange={handleInputChange}/>
+              </Grid>
+              <Grid item xs={6}>
+                <TextField fullWidth label="Last Name" name='lastName' value={formData.lastName} onChange={handleInputChange}/>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField fullWidth label="Email" name='email' value={formData.email} onChange={handleInputChange} />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField fullWidth label="Phone" name='phone' value={formData.phone} onChange={handleInputChange}/>
+              </Grid>
+              <Grid item xs={12}>
+                <Button type='submit' variant="contained" color="success" fullWidth>
+                  Submit
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Box sx={{ pl: 2 }}>
-            <Typography variant="h6" sx={{ mb: 2, fontWeight: 'bold' }}>Contact Information</Typography>
-            {[
-              {
-                name: "Dr. Robert Rozbruch",
-                phone: "347-808-4739",
-                emails: ["deano@hss.edu", "morar@hss.edu"],
-              },
-              {
-                name: "Dr. Austin Fragomen",
-                phone: "347-763-6079",
-                emails: ["fragomena@hss.edu"],
-              },
-              {
-                name: "Dr. Taylor Reif",
-                phone: "347-472-1110",
-                emails: ["Robertsonsh@hss.edu"],
-              },
-              {
-                name: "Dr. Jason Hoellwarth",
-                phone: "213-652-9630",
-                emails: ["hoellwarthj@HSS.EDU"],
-              },
-            ].map((doctor, index) => (
-              <Box key={index} sx={{ mb: 3, p: 2, border: '1px solid #ddd', borderRadius: 2 }}>
-                <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>{doctor.name}</Typography>
-                <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
-                  <PhoneIcon sx={{ mr: 1, color: '#73AD21' }} />
-                  <Typography>{doctor.phone}</Typography>
-                </Box>
-                {doctor.emails.map((email, idx) => (
-                  <Box key={idx} sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
-                    <EmailIcon sx={{ mr: 1, color: '#73AD21' }} />
-                    <Typography>{email}</Typography>
+          <Grid item xs={12} md={6}>
+            <Box sx={{ pl: 2 }}>
+              <Typography variant="h6" sx={{ mb: 2, fontWeight: 'bold' }}>Contact Information</Typography>
+              {[
+                {
+                  name: "Dr. Robert Rozbruch",
+                  phone: "347-808-4739",
+                  emails: ["deano@hss.edu", "morar@hss.edu"],
+                },
+                {
+                  name: "Dr. Austin Fragomen",
+                  phone: "347-763-6079",
+                  emails: ["fragomena@hss.edu"],
+                },
+                {
+                  name: "Dr. Taylor Reif",
+                  phone: "347-472-1110",
+                  emails: ["Robertsonsh@hss.edu"],
+                },
+                {
+                  name: "Dr. Jason Hoellwarth",
+                  phone: "213-652-9630",
+                  emails: ["hoellwarthj@HSS.EDU"],
+                },
+              ].map((doctor, index) => (
+                <Box key={index} sx={{ mb: 3, p: 2, border: '1px solid #ddd', borderRadius: 2 }}>
+                  <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>{doctor.name}</Typography>
+                  <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+                    <PhoneIcon sx={{ mr: 1, color: '#73AD21' }} />
+                    <Typography>{doctor.phone}</Typography>
                   </Box>
-                ))}
-              </Box>
-            ))}
-          </Box>
+                  {doctor.emails.map((email, idx) => (
+                    <Box key={idx} sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+                      <EmailIcon sx={{ mr: 1, color: '#73AD21' }} />
+                      <Typography>{email}</Typography>
+                    </Box>
+                  ))}
+                </Box>
+              ))}
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      </form>
     </Box>
   );
 };
--- src/components/chat/ContactForm.tsx
+++ src/components/chat/ContactForm.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+
 import {
   Box,
   Button,
@@ -11,82 +13,136 @@ import {
   Grid,
 } from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+
+const EMAIL_SERVICE_ID = "service_zeoucv3"
+const EMAIL_TEMPLATE_ID = "template_qiuegmp"
+const EMAIL_USER_PUBLIC_KEY = "zLRxsQKdzbmCEjQfx"
 
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    doctor: '',
+    email: '',
+    subject: '',
+    message: ''
+  });
+
+  const sendEmail = (formData: any) => {
+    const templateParams = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      doctor: formData.doctor,
+      email: formData.email,
+      subject: formData.subject,
+      message: formData.message
+    };
+
+    emailjs.send(EMAIL_SERVICE_ID, EMAIL_TEMPLATE_ID, templateParams, EMAIL_USER_PUBLIC_KEY)
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      }, (error) => {
+        console.log('FAILED...', error);
+      });
+  }
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    sendEmail(formData);
+  };
+
   return (
     <Box sx={{ p: 3, bgcolor: 'white', borderRadius: 2, boxShadow: 3, maxWidth: 900, mx: 'auto' }}>
       <Typography variant="h5" sx={{ mb: 2, fontWeight: 'bold', color: '#73AD21' }}>
         Have a Question?
       </Typography>
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={6}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <FormControl fullWidth>
-                <FormLabel>Which doctor would you like to contact?</FormLabel>
-                <Select defaultValue="">
-                  <MenuItem value="">Choose One</MenuItem>
-                  <MenuItem value="Dr. Robert Rozbruch">Dr. Robert Rozbruch</MenuItem>
-                  <MenuItem value="Dr. Austin Fragomen">Dr. Austin Fragomen</MenuItem>
-                  <MenuItem value="Dr. Taylor Reif">Dr. Taylor Reif</MenuItem>
-                  <MenuItem value="Dr. Jason Hoellwarth">Dr. Jason Hoellwarth</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={6}>
-              <TextField fullWidth label="First Name" required />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField fullWidth label="Last Name" required />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField fullWidth label="Email" required />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField fullWidth label="Subject" required />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField fullWidth label="Message" multiline rows={4} required />
-            </Grid>
-            <Grid item xs={12}>
-              <Button variant="contained" color="success" fullWidth>
-                Submit
-              </Button>
+
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <FormLabel>Which doctor would you like to contact?</FormLabel>
+                  <Select
+                    name="doctor"
+                    value={formData.doctor}
+                    onChange={handleInputChange}
+                    defaultValue=""
+                  >
+                    <MenuItem value="">Choose One</MenuItem>
+                    <MenuItem value="Dr. Robert Rozbruch">Dr. Robert Rozbruch</MenuItem>
+                    <MenuItem value="Dr. Austin Fragomen">Dr. Austin Fragomen</MenuItem>
+                    <MenuItem value="Dr. Taylor Reif">Dr. Taylor Reif</MenuItem>
+                    <MenuItem value="Dr. Jason Hoellwarth">Dr. Jason Hoellwarth</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={6}>
+                <TextField fullWidth label="First Name" required name='firstName' value={formData.firstName} onChange={handleInputChange}/>
+              </Grid>
+              <Grid item xs={6}>
+                <TextField fullWidth label="Last Name" required name='lastName' value={formData.lastName} onChange={handleInputChange}/>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField fullWidth label="Email" required name='email' value={formData.email} onChange={handleInputChange}/>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField fullWidth label="Subject" required name='subject' value={formData.subject} onChange={handleInputChange}/>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField fullWidth label="Message" multiline rows={4} required name='message' value={formData.message} onChange={handleInputChange}/>
+              </Grid>
+              <Grid item xs={12}>
+                <Button type="submit" variant="contained" color="success" fullWidth>
+                  Submit
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Box sx={{ pl: 2 }}>
-            <Typography variant="h6" sx={{ mb: 2, fontWeight: 'bold' }}>Contact Information</Typography>
-            {[
-              {
-                name: "Dr. Robert Rozbruch",
-                email: "rozbruchsr@hss.edu",
-              },
-              {
-                name: "Dr. Austin Fragomen",
-                email: "fragomena@hss.edu",
-              },
-              {
-                name: "Dr. Taylor Reif",
-                email: "reift@hss.edu",
-              },
-              {
-                name: "Dr. Jason Hoellwarth",
-                email: "hoellwarthj@hss.edu",
-              },
-            ].map((doctor, index) => (
-              <Box key={index} sx={{ mb: 3, p: 2, border: '1px solid #ddd', borderRadius: 2 }}>
-                <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>{doctor.name}</Typography>
-                <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
-                  <EmailIcon sx={{ mr: 1, color: '#73AD21' }} />
-                  <Typography>{doctor.email}</Typography>
+          <Grid item xs={12} md={6}>
+            <Box sx={{ pl: 2 }}>
+              <Typography variant="h6" sx={{ mb: 2, fontWeight: 'bold' }}>Contact Information</Typography>
+              {[
+                {
+                  name: "Dr. Robert Rozbruch",
+                  email: "rozbruchsr@hss.edu",
+                },
+                {
+                  name: "Dr. Austin Fragomen",
+                  email: "fragomena@hss.edu",
+                },
+                {
+                  name: "Dr. Taylor Reif",
+                  email: "reift@hss.edu",
+                },
+                {
+                  name: "Dr. Jason Hoellwarth",
+                  email: "hoellwarthj@hss.edu",
+                },
+              ].map((doctor, index) => (
+                <Box key={index} sx={{ mb: 3, p: 2, border: '1px solid #ddd', borderRadius: 2 }}>
+                  <Typography variant="subtitle1" sx={{ fontWeight: 'bold' }}>{doctor.name}</Typography>
+                  <Box sx={{ display: 'flex', alignItems: 'center', mt: 1 }}>
+                    <EmailIcon sx={{ mr: 1, color: '#73AD21' }} />
+                    <Typography>{doctor.email}</Typography>
+                  </Box>
                 </Box>
-              </Box>
-            ))}
-          </Box>
+              ))}
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      </form>
     </Box>
   );
 };
